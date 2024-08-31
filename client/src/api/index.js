b9e26bbbc9db4625b0e7c33cd1a3e15e78b95228
rithvik reddy <147538307+rithvikreddybasani@users.noreply.github.com>
2024-08-31 16:42:40 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'https://share-purse.vercel.app/' });
+const API = axios.create({ baseURL: 'https://sharepurse-1.onrender.com/' });
 
 // const API = axios.create({ baseURL: ''})
 // 
