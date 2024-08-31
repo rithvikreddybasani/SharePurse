099b6b1b744dd101ed26726815acc524057e9ad2
rithvik reddy <147538307+rithvikreddybasani@users.noreply.github.com>
2024-08-31 09:50:39 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 const logger = require('./model/helper/logger')
 const requestLogger =  require('./model/helper/requestLogger')
-const apiAuth =  require('./model/helper/apiAuthentication') //middleware to validate token
+const apiAuth =  require('./model/helper/apiAuthentication') 
 
 
 const usersRouter = require('./routes/userRouter')
@@ -17,25 +17,12 @@ const groupRouter = require('./routes/groupRouter')
 const expenseRouter = require('./routes/expenseRouter')
  
 app.use(requestLogger)
-
-
-
-
 app.use('/api/users', usersRouter)
 app.use('/api/group', apiAuth.validateToken,groupRouter)
 app.use('/api/expense', apiAuth.validateToken,expenseRouter)
 
-//To detect and log invalid api hits 
-app.all('*', (req, res) => {
-    logger.error(`[Invalid Route] ${req.originalUrl}`)
-    res.status(404).json({
-        status: 'fail',
-        message: 'Invalid path'
-      })
-})
 
 const port = process.env.PORT || 5000
 app.listen(port, (err) => {
     console.log(`Server started in PORT | ${port}`)
-    logger.info(`Server started in PORT | ${port}`)
 })
