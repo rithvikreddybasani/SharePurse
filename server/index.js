232e57cb539400cac897a1fc1c1ab0ecac51f284
rithvik <basanirithvikreddy2@gmail.com>
2024-08-31 12:47:03 +0530

    other

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ const corsConfig={
     Credential:true,
     methods:["GET","POST","PUT","DELETE"],
 };
-app.options("",cors(corsConfig))
 app.use(express.json())
 app.use(cors(corsConfig))
+app.use(express.json())
 const logger = require('./model/helper/logger')
 const requestLogger =  require('./model/helper/requestLogger')
 const apiAuth =  require('./model/helper/apiAuthentication') 
